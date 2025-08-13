cfff3108b8f3da8e103546098ba3c6d75c5d79f1
j06046804-lang <j06046804@gmail.com>
2025-08-13 10:43:03 -0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,8 @@
 // webhook.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
@@ -118,4 +118,5 @@ export default async function handler(req, res) {
     return res.status(500).json({ message: "Error interno del servidor" });
   }
 }
+
 
